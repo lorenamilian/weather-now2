fbd5bf28c8a9056e76604af32d774d8c6a7e56fb
lorena <lorenalies2312@gmail.com>
2019-11-04 11:16:08 -0500

    setting view engine

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+ 
+//setting view engine
+app.set('view engine', 'ejs')
+
+//ROUTES
+ 
+//get
+app.get('/', function (req, res) {
+  res.render('home.ejs');
+})
+ 
+app.listen(3000, function(){
+  console.log("my server is running better")
+})
